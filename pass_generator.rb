0ec603b5ef6ead9ed62b2ee057ddbe74edd03ee2
Jonathan Norton <jonathan.norton@synechron.com>
2019-02-21 11:23:18 -0500

    pas generator

---
+++ pass_generator.rb
@@ -0,0 +1,23 @@
+require 'securerandom'
+random_string = SecureRandom.hex
+
+puts "\n"*5
+puts "*"*40
+puts "*"*40
+puts "*"*40
+puts "*"*40
+puts "*"*40
+puts "\n"
+puts "\n"
+
+print "Your Password is: "
+
+puts random_string
+puts "\n"
+puts "\n"
+puts "*"*40
+puts "*"*40
+puts "*"*40
+puts "*"*40
+puts "*"*40
+puts "\n"*5
