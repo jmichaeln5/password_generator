7d55700ea93b7fe39351f2652000a448f57a5e56
Jonathan Norton <jonathan.norton@synechron.com>
2019-02-21 11:35:03 -0500

    completely refactored

--- pass_generator.rb
+++ pass_generator.rb
@@ -1,7 +1,10 @@
 require 'securerandom'
 random_string = SecureRandom.hex
 
-puts "\n"*5 + "*"*40 + "\n"*2
+space = "\n"*5 + "*"*40 + "\n"*2
+
+print space
 print "Your Password is: "
 print random_string
-puts "\n"*2 + "*"*40 + "\n"*5
+space.reverse!
+print space
